b65dd6b29c675c003045cbf53932c7f3ad97860d
nixjoe <nixjoe@hotmail.com>
2024-08-09 03:56:14 +0800

    mapper2

--- NES/fpganes/src/mmu.v
+++ NES/fpganes/src/mmu.v
@@ -192,6 +192,125 @@ assign vram_a10 = flags[14] ? chr_ain[10] : chr_ain[11];
 
 endmodule
 
+module Mapper2(input clk, input ce, input reset,
+                input [31:0] flags,
+                input [15:0] prg_ain, output [21:0] prg_aout,
+                input prg_read, prg_write,                   // Read / write signals
+                input [7:0] prg_din,
+                output prg_allow,                            // Enable access to memory for the specified operation.
+                input [13:0] chr_ain, output [21:0] chr_aout,
+                output chr_allow,                      // Allow write
+                output reg vram_a10,                         // Value for A10 address line
+                output vram_ce);                             // True if the address should be routed to the internal 2kB VRAM.
+    // reg [3:0] a53prg;    // output PRG ROM (A14-A20 on ROM)
+    // reg [1:0] a53chr;    // output CHR RAM (A13-A14 on RAM)
+  
+    // reg [3:0] inner;    // "inner" bank at 01h
+    // reg [5:0] mode;     // mode register at 80h
+    reg [2:0] outer;    // "outer" bank at 81h  
+    // reg [1:0] selreg;   // selector register
+    
+    // Allow writes to 0x5000 only when launching through the proper mapper ID.
+    wire [7:0] mapper = flags[7:0];
+    // wire allow_select = (mapper == 8'd28);
+        
+    always @(posedge clk) if (reset) begin
+      // mode[5:2] <= 0;         // NROM mode, 32K mode
+      outer[2:0] <= 3'b111;    // last bank
+      // outer[2:0] <= 0;    // last bank
+      // a53prg[3:0] <= 4'hf;
+      // inner <= 0;
+      // selreg <= 1;
+      
+      // Set value for mirroring
+      // if (mapper == 2 || mapper == 0 || mapper == 3)
+      //   mode[1:0] <= flags[14] ? 2'b10 : 2'b11;
+        
+      // UNROM #2 - Current bank in $8000-$BFFF and fixed top half of outer bank in $C000-$FFFF
+      // if (mapper == 2)
+      //   mode[5:2] <= 4'b1111;
+        
+      // CNROM #3 - Fixed PRG bank, switchable CHR bank.
+      // if (mapper == 3)
+      //   selreg <= 0;
+
+      // AxROM #7 - Switch 32kb rom bank + switchable nametables
+      // if (mapper == 7) begin
+      //   mode[1:0] <= 2'b00;   // Switchable VRAM page.
+      //   mode[5:2] <= 4'b1100; // 256K banks, (B)NROM mode
+      // end
+
+    end else if (ce) begin
+      // if ((prg_ain[15:12] == 4'h5) & prg_write && allow_select) selreg <= {prg_din[7], prg_din[0]};        // select register
+      // if (prg_ain[15] & prg_write) begin
+      //   case (selreg)
+      //   2'h0:  {mode[0], a53chr}  <= {(mode[1] ? mode[0] : prg_din[4]), prg_din[1:0]};  // CHR RAM bank
+      //   2'h1:  {mode[0], inner}   <= {(mode[1] ? mode[0] : prg_din[4]), prg_din[3:0]};  // "inner" bank
+      //   2'h2:  {mode}             <= {prg_din[5:0]};                                    // mode register
+      //   2'h3:  {outer}            <= {prg_din[5:0]};                                    // "outer" bank
+      //   endcase
+      // end
+      if (prg_ain[15] & prg_write) begin
+        {outer}            <= {prg_din[2:0]};                                    // "outer" bank
+      end
+    end
+    
+    // always @(*) begin
+    //   // mirroring mode
+    //   // casez(mode[1:0])
+    //   // 2'b0?   :   vram_a10 = {mode[0]};        // 1 screen lower
+    //   // 2'b10   :   vram_a10 = {chr_ain[10]};    // vertical
+    //   // 2'b11   :   vram_a10 = {chr_ain[11]};    // horizontal
+    //   // endcase
+
+    //   // PRG ROM bank size select
+    //   // casez({mode[5:2], prg_ain[14]})
+    //   // 5'b00_0?_?  :  a53prg = {outer[5:0],             prg_ain[14]};  // 32K banks, (B)NROM mode
+    //   // 5'b01_0?_?  :  a53prg = {outer[5:1], inner[0],   prg_ain[14]};  // 64K banks, (B)NROM mode
+    //   // 5'b10_0?_?  :  a53prg = {outer[5:2], inner[1:0], prg_ain[14]};  // 128K banks, (B)NROM mode
+    //   // 5'b11_0?_?  :  a53prg = {outer[5:3], inner[2:0], prg_ain[14]};  // 256K banks, (B)NROM mode
+      
+    //   // 5'b00_10_1,
+    //   // 5'b00_11_0  :  a53prg = {outer[5:0], inner[0]};             // 32K banks, UNROM mode
+    //   // 5'b01_10_1,
+    //   // 5'b01_11_0  :  a53prg = {outer[5:1], inner[1:0]};           // 64K banks, UNROM mode
+    //   // 5'b10_10_1,
+    //   // 5'b10_11_0  :  a53prg = {outer[5:2], inner[2:0]};           // 128K banks, UNROM mode
+    //   // 5'b11_10_1,
+    //   // 5'b11_11_0  :  a53prg = {outer[5:3], inner[3:0]};           // 256K banks, UNROM mode
+      
+    //   // default     :  a53prg = {outer[5:0],             prg_ain[14]};  // 16K fixed bank
+    //   // endcase
+    //   if (prg_ain[14]) begin
+    //     a53prg = 4'b1111;
+    //   end else begin
+    //     a53prg = {outer[2:0],prg_ain[14]};
+    //   end
+
+    // end
+
+  // assign vram_ce = chr_ain[13];
+  // 
+  // assign prg_aout = {1'b0, (a53prg & 7'b0011111), prg_ain[13:0]};
+  // assign prg_allow = prg_ain[15] && !prg_write;
+  // assign chr_allow = flags[15];
+  //                               [14:13]  
+  // assign chr_aout = {7'b10_0000_0, a53chr, chr_ain[12:0]};
+  //                                 [6:0]
+
+  // assign prg_aout = {4'b0000, a53prg , prg_ain[13:0]};
+  assign prg_aout = {5'b00000, (prg_ain[14] ? 4'b111 : outer[2:0]), prg_ain[13:0]};
+  // assign prg_aout = {7'b00_0000_0, prg_ain[14:0]};
+  assign prg_allow = prg_ain[15] && !prg_write;
+  assign chr_allow = flags[15];
+
+  // assign chr_aout = {9'b11_1111_111, chr_ain[12:0]};
+  assign chr_aout = {9'b10_0000_000, chr_ain[12:0]};
+
+  assign vram_ce = chr_ain[13];
+  assign vram_a10 = flags[14] ? chr_ain[10] : chr_ain[11];
+endmodule
+
 // MMC1 mapper chip. Maps prg or chr addresses into a linear address.
 // If vram_ce is set, {vram_a10, chr_aout[9:0]} are used to access the NES internal VRAM instead.
 module MMC1(input clk, input ce, input reset,
@@ -1746,10 +1865,15 @@ module MultiMapper(input clk, input ce, input ppu_ce, input reset,
   MMC0 mmc0(clk, ce, flags, prg_ain, mmc0_prg_addr, prg_read, prg_write, prg_din, mmc0_prg_allow,
                             chr_ain, mmc0_chr_addr, mmc0_chr_allow, mmc0_vram_a10, mmc0_vram_ce);
 
-  wire map30_prg_allow, map30_vram_a10, map30_vram_ce, map30_chr_allow;
-  wire [21:0] map30_prg_addr, map30_chr_addr;
-  Mapper30 map30(clk, ce, reset, flags, prg_ain, map30_prg_addr, prg_read, prg_write, prg_din, map30_prg_allow,
-                                   chr_ain, map30_chr_addr,map30_chr_allow,map30_vram_a10, map30_vram_ce );
+  wire map2_prg_allow, map2_vram_a10, map2_vram_ce, map2_chr_allow;
+  wire [21:0] map2_prg_addr, map2_chr_addr;
+  Mapper2 map2(clk, ce, reset, flags, prg_ain, map2_prg_addr, prg_read, prg_write, prg_din, map2_prg_allow,
+                                   chr_ain, map2_chr_addr,map2_chr_allow,map2_vram_a10, map2_vram_ce );
+
+  // wire map30_prg_allow, map30_vram_a10, map30_vram_ce, map30_chr_allow;
+  // wire [21:0] map30_prg_addr, map30_chr_addr;
+  // Mapper30 map30(clk, ce, reset, flags, prg_ain, map30_prg_addr, prg_read, prg_write, prg_din, map30_prg_allow,
+  //                                  chr_ain, map30_chr_addr,map30_chr_allow,map30_vram_a10, map30_vram_ce );
 
   wire mmc1_prg_allow, mmc1_vram_a10, mmc1_vram_ce, mmc1_chr_allow;
   wire [21:0] mmc1_prg_addr, mmc1_chr_addr;
@@ -1877,9 +2001,11 @@ module MultiMapper(input clk, input ce, input ppu_ce, input reset,
     chr_dout = mmc5_chr_dout;
         
     case(flags[7:0])
-    0, 2, 3, 7,
+    2:  {prg_aout, prg_allow, chr_aout, vram_a10, vram_ce, chr_allow}      = {map2_prg_addr, map2_prg_allow, map2_chr_addr, map2_vram_a10, map2_vram_ce, map2_chr_allow};
+    // 0, 2, 3, 7,
+    0, 3, 7,
     28: {prg_aout, prg_allow, chr_aout, vram_a10, vram_ce, chr_allow}      = {map28_prg_addr, map28_prg_allow, map28_chr_addr, map28_vram_a10, map28_vram_ce, map28_chr_allow};
-    30: {prg_aout, prg_allow, chr_aout, vram_a10, vram_ce, chr_allow}      = {map30_prg_addr, map30_prg_allow, map30_chr_addr, map30_vram_a10, map30_vram_ce, map30_chr_allow};
+    // 30: {prg_aout, prg_allow, chr_aout, vram_a10, vram_ce, chr_allow}      = {map30_prg_addr, map30_prg_allow, map30_chr_addr, map30_vram_a10, map30_vram_ce, map30_chr_allow};
     1:  {prg_aout, prg_allow, chr_aout, vram_a10, vram_ce, chr_allow}      = {mmc1_prg_addr, mmc1_prg_allow, mmc1_chr_addr, mmc1_vram_a10, mmc1_vram_ce, mmc1_chr_allow};
     9:  {prg_aout, prg_allow, chr_aout, vram_a10, vram_ce, chr_allow}      = {mmc2_prg_addr, mmc2_prg_allow, mmc2_chr_addr, mmc2_vram_a10, mmc2_vram_ce, mmc2_chr_allow};
     118, // TxSROM connects A17 to CIRAM A10.
